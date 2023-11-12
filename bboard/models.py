78c6d238937acae14d5cdc4cafffd0e733278e0d
shark826 <gs20062@yandex.ru>
2023-11-12 19:15:25 +0300

    g1.14_1.13

--- bboard/models.py
+++ bboard/models.py
@@ -3,7 +3,12 @@ from django.db import models
 # Create your models here.
 
 class Bb(models.Model):
-    title = models.CharField(max_length=50)
-    content = models.TextField()
-    price = models.FloatField()
-    published = models.DateTimeField(auto_now_add=True, db_index=True) 
+    title = models.CharField(max_length=50, verbose_name='Товар')
+    content = models.TextField(verbose_name='Описание')
+    price = models.FloatField(verbose_name='Цена')
+    published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Опубликовано') 
+
+    class Meta:
+        verbose_name_plural = 'Объявления'
+        verbose_name = 'Объявление'
+        ordering = ['-published']
