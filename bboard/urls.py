e59a377903647c60ca699918568c97c2c6714bdb
shark826 <gs20062@yandex.ru>
2023-11-13 20:39:11 +0300

    g2.3_2.5

--- bboard/urls.py
+++ bboard/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, rubric_bbs
 
 urlpatterns = [
+    path('<int:rubric_id>/', rubric_bbs),
     path('', index)
 ]
--- bboard/views.py
+++ bboard/views.py
@@ -1,8 +1,24 @@
 # Create your views here.
 from django.shortcuts import render
 
-from .models import Bb
+from .models import Bb, Rubric
 
 def index(request):
     bbs = Bb.objects.all()
-    return render(request, 'bboard/index.html', {'bbs': bbs})
+    rubrics = Rubric.objects.all()
+    context = {
+        'bbs': bbs,
+        'rubrics': rubrics,
+    }
+    return render(request, 'bboard/index.html', context)
+
+def rubric_bbs(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {
+        'bbs': bbs,
+        'rubrics': rubrics,
+        'current_rubric': current_rubric
+    }
+    return render(request, 'bboard/rubric_bbs.html', context)
