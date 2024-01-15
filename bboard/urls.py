5e0451c488b71661e4e116e1fa47355a6a4b44f3
Roman_work <013AntonovRA@16.sfr.gov.ru>
2024-01-15 10:58:00 +0300

    2.4 urls

--- bboard/urls.py
+++ bboard/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import index, rubric_bbs
 
 urlpatterns = [
-    path('<int:rubric_id>/', rubric_bbs),
-    path('', index)
+    path('<int:rubric_id>/', rubric_bbs, name='rubric_bbs'),
+    path('', index, name='index')
 ]
